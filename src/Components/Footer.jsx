547a943bc2b5157a1bc551cb663fe566d93b9185
Julio Munoz <julioc.munozg@gmail.com>
2024-07-31 20:02:09 -0400

    Initial commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Footer = () => {
+    return(
+        <footer>
+            <h1>This is a Footer</h1>
+            <p>For any doubt don't hesitate to contact us!</p>
+        </footer>
+    )
+}
+
+export default Footer
